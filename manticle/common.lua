632688184fa5849e71c0bb00aec31e59ecfa8459
Nick <njd5475@gmail.com>
2018-08-11 13:27:00 -0400

    Cleans up game, removing legacy games codes.

--- manticle/common.lua
+++ manticle/common.lua
@@ -1,7 +1,5 @@
 require('engine.common')
-Platform = require('objects.platform')
-Litterbox = require('objects.litterbox')
-MilkAndCookies = require('objects.milkandcookies')
+
 HouseBrick = require('objects.housebrick')
 Vine = require('objects.vine')
 Player = require('objects.player')
--- manticle/objects/catgenerator.lua
+++
@@ -1,9 +0,0 @@
-
-require('engine.common')
-local generator = GameObject:derive("catgenerator")
-
-function generator:update(game, dt)
-
-end
-
-return generator
